1e39ee38720ddee25ca80036029fb95ac8150098
hectorpina123@gmail.com <hectorpina123@gmail.com>
2015-06-16 14:42:46 -0600

    subtracting bills from momey

--- app/assets/javascripts/personal-budget.js
+++ app/assets/javascripts/personal-budget.js
@@ -1,20 +1,25 @@
-var moneyinput =document.getElementById('money_made');
+var moneyInput =document.getElementById('money_made');
 var moneyButton =document.getElementById('money_made_button');
 var totalMoney=0;
 var total =document.getElementById('total');
 
 
 moneyButton.addEventListener('click',function(){
-  totalMoney =moneyinput.value;
+  totalMoney =moneyInput.value;
   console.log(totalMoney);
 });
 
-var billinput =document.getElementById('money_spent');
+var billInput =document.getElementById('money_spent');
+var billInput2 =document.getElementById('money_spent2');
+var billInput3 =document.getElementById('money_spent3');
 var billButton =document.getElementById('money_spent_button');
 var totalBill=0;
 
+
+
 billButton.addEventListener('click',function(){
-  totalBill =billinput.value;
+  totalBill = parseInt(billInput.value) + parseInt(billInput2.value) 
+  + parseInt(billInput3.value);
   console.log(totalBill);
   total.innerHTML= totalMoney - totalBill;
 });
