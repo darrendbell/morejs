cf019f254b536802e1a15529f753bf9eb2692367
hectorpina123@gmail.com <hectorpina123@gmail.com>
2015-06-16 12:06:37 -0600

    first commit first buttons

--- app/assets/javascripts/personal-budget.js
+++ app/assets/javascripts/personal-budget.js
@@ -1,8 +1,21 @@
 var moneyinput =document.getElementById('money_made');
 var moneyButton =document.getElementById('money_made_button');
 var totalMoney=0;
+var total =document.getElementById('total');
+
 
 moneyButton.addEventListener('click',function(){
   totalMoney =moneyinput.value;
   console.log(totalMoney);
 });
+
+var billinput =document.getElementById('money_spent');
+var billButton =document.getElementById('money_spent_button');
+var totalBill=0;
+
+billButton.addEventListener('click',function(){
+  totalBill =billinput.value;
+  console.log(totalBill);
+  total.innerHTML= totalMoney - totalBill;
+});
+
